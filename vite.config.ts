a99be8dcdbb9fba69e9b67161cdd952069a71764
seokmin <msm0748@gmail.com>
2024-11-16 20:35:59 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': {}, // process 객체를 빈 값으로 정의
+  },
+});
